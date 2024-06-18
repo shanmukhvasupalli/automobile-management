a2552f963840fdf10ffbc32d7b2d0aedbca1fc5b
Shanmukh Vasupalli <147924962+shanmukhvasupalli@users.noreply.github.com>
2024-06-18 18:02:56 +0530

    Update Server.js

--- backendapp/Server.js
+++ backendapp/Server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
-const dburl = process.env.DB_URL
+//const dburl = process.env.DB_URL
+const dburl = "mongodb+srv://shannu:shannu@cluster0.pxfynep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 mongoose.connect(dburl).then(() => {
     console.log("Connected to DB Successfully")
 }).catch((err) => {
